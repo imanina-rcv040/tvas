a66984de430a3f03eb8db32cb1a4a556c832db8d
Imanina Majeed <imanina@recovision.net>
2023-04-12 14:27:49 +0800

    feat(mqtt): create connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 // import libraries
 import { Admin, Resource } from "react-admin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import mqtt from "mqtt/dist/mqtt";
 
 // import components
 import { MyLogin } from "./layout/Login";
@@ -15,6 +16,16 @@ import "@fontsource/poppins"; // Defaults to weight 400.
 const ffmpegIP = "172.17.11.7";
 console.log("ffmpegIP:", ffmpegIP, "connected");
 
+// set and connect MQTT
+const REACT_APP_MQTT_SERVER =
+  process.env.REACT_APP_MQTT_SERVER || "ws://172.17.0.216:8080/";
+const REACT_APP_MQTT_DATA_TOPIC =
+  process.env.REACT_APP_MQTT_DATA_TOPIC || "uturn/event";
+const client = mqtt.connect(REACT_APP_MQTT_SERVER);
+console.log("REACT_APP_MQTT_SERVER", REACT_APP_MQTT_SERVER);
+console.log("REACT_APP_MQTT_DATA_TOPIC", REACT_APP_MQTT_DATA_TOPIC);
+console.log("MQTT: client connected!");
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -24,7 +35,13 @@ export default function App() {
           element={
             <Admin
               title="Traffic Offence"
-              dashboard={() => <MyDashboard ffmpegIP={ffmpegIP} />}
+              dashboard={() => (
+                <MyDashboard
+                  ffmpegIP={ffmpegIP}
+                  mqttClient={client}
+                  mqttTopic={REACT_APP_MQTT_DATA_TOPIC}
+                />
+              )}
               loginPage={MyLogin}
               layout={MyLayout}
               disableTelemetry
