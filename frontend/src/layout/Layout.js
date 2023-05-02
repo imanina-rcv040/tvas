dc9146b2ebcc4c689654d20125928a2a9598a641
Imanina Majeed <imanina@recovision.net>
2023-05-02 11:50:01 +0800

    feat: Add livestream component into layout

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -4,7 +4,21 @@ import { Layout } from "react-admin";
 // import components
 import { MyAppBar } from "./AppBar";
 import { MyMenu } from "./Menu";
+import { useLocation } from "react-router-dom";
+import { MyLivestream } from "../livestream/Livestream";
 
-export const MyLayout = (props) => (
-  <Layout {...props} appBar={MyAppBar} menu={MyMenu} />
-);
+export const MyLayout = (props) => {
+  const location = useLocation();
+
+  // Render the MyLivestream component on the dashboard route
+  if (location.pathname === "/livestream") {
+    return (
+      <Layout {...props} appBar={MyAppBar} menu={MyMenu}>
+        <MyLivestream {...props} />
+      </Layout>
+    );
+  }
+
+  // Render the default layout for other routes
+  return <Layout {...props} appBar={MyAppBar} menu={MyMenu} />;
+};
