9448f6ed18620922ad827bad5b6d7e520b4e2abb
ckchan <ckchan@recovision.net>
2023-05-15 14:37:21 +0800

    build:  Assign 2 tagging to the same image

--- .dev/build-docker.sh
+++ .dev/build-docker.sh
@@ -2,24 +2,26 @@
 
 set -e
 
+docker_image_reponame=recocloudapp/recocloud:tvas
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # docker build -t recocloud-app/tvas:v1.0.0 "$SCRIPT_DIR"/../
 
 
-# git_version="$(git describe --tags --exact-match --match "v*.*.*"\
-#  || git describe --match "v*.*.*" --tags\
-#  || git describe --tags\
-#  || git rev-parse HEAD)"
+git_version="$(git describe --tags --exact-match --match "v*.*.*"\
+ || git describe --match "v*.*.*" --tags\
+ || git describe --tags\
+ || git rev-parse HEAD)"
 
-git_version="$(git describe --tags --abbrev=0)"
+current_tagname="$docker_image_reponame":"$git_version"
+latest_tagname="$docker_image_reponame":latest
 
 export DOCKER_BUILDKIT=1
 
-echo "Building recocloudapp/recocloud:tvas-$git_version"
+echo "Building ${docker_image_reponame}-$git_version"
 docker build\
- -t recocloudapp/recocloud:tvas-"$git_version"\
+ -t $current_tagname\
  -f "$SCRIPT_DIR"/../frontend/dockerfile\
  .
-
+docker tag "$current_tagname" "$latest_tagname"
  echo "Jobs done!"
