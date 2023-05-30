f54b02c0872069a3ce93931b5ed2e5959f53c1f2
Imanina Majeed <imanina@recovision.net>
2023-05-30 09:44:54 +0800

    feat: Add state variables to  component

--- frontend/src/page/CameraFeed/MyCameraFeed.js
+++ frontend/src/page/CameraFeed/MyCameraFeed.js
@@ -23,3 +23,20 @@ const REACT_APP_BACKEND_TVAS_SERVER =
   process.env.REACT_APP_BACKEND_TVAS_SERVER || "172.17.0.143:20001";
 console.log("REACT_APP_BACKEND_TVAS_SERVER:", REACT_APP_BACKEND_TVAS_SERVER);
 
+export const MyCameraFeed = () => {
+  const [openDialog, setOpenDialog] = useState(false);
+  const [alert, setAlert] = useState(false);
+
+  const [camInfo, setCamInfo] = useState(null);
+  const [cameras, setCameras] = useState([]);
+  const [selectedCameraName, setSelectedCameraName] = useState(null);
+
+  const [latestEvent, setLatestEvent] = useState(null);
+
+  const [violationInfo, setViolationInfo] = useState({
+    eventType: "Speeding",
+    licensePlate: "WYL9335",
+    licensePlateImage: "https://example.com/license_plate_image.jpg",
+    vehicleImage: "https://example.com/vehicle_image.jpg",
+    timestamp: "May 26, 2023 - 2:30 PM",
+  });
