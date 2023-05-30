c77da6ea7dfc5f2b6f8fa4a9865ec541d3c00b31
Imanina Majeed <imanina@recovision.net>
2023-05-30 09:41:31 +0800

    feat: Add import statements to component

---
+++ frontend/src/page/CameraFeed/MyCameraFeed.js
@@ -0,0 +1,19 @@
+// import libraries
+import { useState, useEffect } from "react";
+import {
+  Alert,
+  Card,
+  CardContent,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Typography,
+} from "@mui/material";
+
+// import components
+import { MyPageHeader } from "./component/MyPageHeader";
+
+// import styling
+import "./MyCameraFeed.css";
