8eec0ea6cba58bf00432a1bf9e79b04b1b3d940f
Imanina Majeed <imanina@recovision.net>
2023-05-30 09:41:44 +0800

    chore: Set backend server configuration

--- frontend/src/page/CameraFeed/MyCameraFeed.js
+++ frontend/src/page/CameraFeed/MyCameraFeed.js
@@ -17,3 +17,9 @@ import { MyPageHeader } from "./component/MyPageHeader";
 
 // import styling
 import "./MyCameraFeed.css";
+
+// set backend server
+const REACT_APP_BACKEND_TVAS_SERVER =
+  process.env.REACT_APP_BACKEND_TVAS_SERVER || "172.17.0.143:20001";
+console.log("REACT_APP_BACKEND_TVAS_SERVER:", REACT_APP_BACKEND_TVAS_SERVER);
+
