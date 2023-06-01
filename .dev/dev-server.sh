ecec5eb949051fdcbf10f6f73e59e8659e82dbec
Ho Zi Xuan <zixuanho@recogine.com>
2023-06-01 16:26:56 +0800

    feat: update dev script according docker path

--- .dev/dev-server.sh
+++ .dev/dev-server.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 "$SCRIPT_DIR"/stop-server.sh
 
 # Start the development server
-docker-compose -f "$SCRIPT_DIR"/../frontend/docker-compose.yml -f "$SCRIPT_DIR"/../frontend/docker-compose.dev.yml up
+docker-compose -f "$SCRIPT_DIR"/../docker-compose.yml -f "$SCRIPT_DIR"/../docker-compose.dev.yml up
 
 # Perform server cleanup
-docker-compose -f "$SCRIPT_DIR"/../frontend/docker-compose.yml -f "$SCRIPT_DIR"/../frontend/docker-compose.dev.yml down
+docker-compose -f "$SCRIPT_DIR"/../docker-compose.yml -f "$SCRIPT_DIR"/../docker-compose.dev.yml down
--- .dev/run-server.sh
+++ .dev/run-server.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "WARNING: FOR DEVELOPMENT PURPOSE ONLY! DO NOT USE OR COPY THIS SCRIPT TO PRODUCTION!"
+echo "NOTE: Make sure that you have docker build all the images!"
 
-docker-compose -f "$SCRIPT_DIR"/../frontend/docker-compose.yml up -d
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
+echo "[1] --- Up server"
+docker-compose -f "$SCRIPT_DIR"/../docker-compose.yml up -d
+
+echo
+echo "Finished setting up the servers. Please check with: docker ps && docker logs"
