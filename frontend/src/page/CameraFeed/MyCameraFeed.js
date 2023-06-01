765e9dffc60f68ca04b3e7b4cac1ccf1a1b82385
Imanina Majeed <imanina@recovision.net>
2023-06-01 11:24:40 +0800

    refactor: Update variable declaration

--- frontend/src/page/CameraFeed/MyCameraFeed.js
+++ frontend/src/page/CameraFeed/MyCameraFeed.js
@@ -22,7 +22,7 @@ import "./MyCameraFeed.css";
 const REACT_APP_BACKEND_TVAS_SERVER =
   process.env.REACT_APP_BACKEND_TVAS_SERVER || "http://172.17.0.143:20001";
 
-let backendServerURL = REACT_APP_BACKEND_TVAS_SERVER;
+const backendServerURL = REACT_APP_BACKEND_TVAS_SERVER;
 console.log("API URL:", backendServerURL);
 
 export const MyCameraFeed = () => {
