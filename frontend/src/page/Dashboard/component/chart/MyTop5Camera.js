694373998d994b76a44b4686146c7389f0b6c03b
Imanina Majeed <imanina@recovision.net>
2023-06-09 18:18:43 +0800

    feat: Fetch data from backend

--- frontend/src/page/Dashboard/component/chart/MyTop5Camera.js
+++ frontend/src/page/Dashboard/component/chart/MyTop5Camera.js
@@ -22,3 +22,24 @@ const colors = ["#00C49F", "#FFBB28", "#0088FE", "#8884d8", "#ff8042"];
 
   const [topCamViolation, setTopCamViolation] = useState([]);
 
+  // Fetch cams with most violations
+  useEffect(() => {
+    const fetchTopCamViolation = async () => {
+      try {
+        const apiURL = `${backendServerURL}`;
+        const response = await fetch(apiURL);
+        if (response.ok) {
+          const responseData = await response.json();
+          console.log("Data for camera violation report:", responseData);
+          setTopCamViolation(responseData);
+        } else {
+          console.log("Error response camera:", response.status);
+        }
+      } catch (error) {
+        console.log("Error fetch camera violations", error);
+      }
+    };
+
+    fetchTopCamViolation();
+    console.log("topCamViolation", topCamViolation);
+  }, []);
