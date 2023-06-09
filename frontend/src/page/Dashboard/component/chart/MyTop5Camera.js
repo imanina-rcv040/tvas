9d2e906251ee501c213b49d5e7f73406d2671e2c
Imanina Majeed <imanina@recovision.net>
2023-06-09 18:15:22 +0800

    feat: Add library import statements

---
+++ frontend/src/page/Dashboard/component/chart/MyTop5Camera.js
@@ -0,0 +1,13 @@
+// import libraries
+import { useState, useEffect } from "react";
+import { Grid, Card, CardContent, Typography } from "@mui/material";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
