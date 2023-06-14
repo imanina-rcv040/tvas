7941ec1f03a8ae910075ca8203f2fec0a38f2d65
Imanina Majeed <imanina@recovision.net>
2023-06-14 17:23:01 +0800

    refactor: Use fetched data for filter selection

--- frontend/src/page/History/MyHistory.js
+++ frontend/src/page/History/MyHistory.js
@@ -31,33 +31,78 @@ const backendServerURL = `${REACT_APP_BACKEND_TVAS_SERVER}`;
 // set image path
 const imgURL = `${REACT_APP_BACKEND_FILE_SERVER}/evidence/`;
 
-const HistoryFilters = (props) => (
-  <Filter {...props}>
-    <SearchInput source="q" alwaysOn />
-    <ReferenceInput source="typeEvent" reference="history" label="Event">
-      <SelectInput
-        source="typeEvent"
-        label="Event Type"
-        choices={[
-          { id: "illegal-uturn", name: "illegal-uturn" },
-          { id: "illegal-parking", name: "illegal-parking" },
-        ]}
-      />
-    </ReferenceInput>
-    <ReferenceInput source="cameraId" label="Camera" reference="history">
-      <SelectInput
-        source="cameraId"
-        label="Camera ID"
-        choices={[
-          { id: 1, name: "1" },
-          { id: 2, name: "2" },
-        ]}
-      />
-    </ReferenceInput>
-    <DateInput source="snapTimestamp_start" label="Start Date" />
-    <DateInput source="snapTimestamp_end" label="End Date" />
-  </Filter>
-);
+const HistoryFilters = (props) => {
+  const [eventSelection, setEventSelection] = useState([]);
+  const [cameraSelection, setCameraSelection] = useState([]);
+
+  useEffect(() => {
+    // Fetch event types from the API
+    const fetchEvents = async () => {
+      try {
+        const apiURL = `${backendServerURL}/history/type-event`;
+        const response = await fetch(apiURL);
+        if (response.ok) {
+          const responseData = await response.json();
+          setEventSelection(responseData);
+        } else {
+          console.log("Error response:", response.status);
+        }
+      } catch (error) {
+        console.log("Error fetch event type:", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  useEffect(() => {
+    // Fetch event types from the API
+    const fetchCameras = async () => {
+      try {
+        const apiURL = `${backendServerURL}/history/camera`;
+        const response = await fetch(apiURL);
+        if (response.ok) {
+          const responseData = await response.json();
+          setCameraSelection(responseData);
+        } else {
+          console.log("Error response:", response.status);
+        }
+      } catch (error) {
+        console.log("Error fetch camera for filter selection:", error);
+      }
+    };
+
+    fetchCameras();
+  }, []);
+
+  return (
+    <Filter {...props}>
+      <SearchInput source="q" alwaysOn />
+      <ReferenceInput source="typeEvent" reference="history" label="Event">
+        <SelectInput
+          source="typeEvent"
+          label="Event Type"
+          choices={eventSelection.map((eventType) => ({
+            id: eventType.id,
+            name: eventType.name,
+          }))}
+        />
+      </ReferenceInput>
+      <ReferenceInput source="cameraId" label="Camera" reference="history">
+        <SelectInput
+          source="cameraId"
+          label="Camera ID"
+          choices={cameraSelection.map((camera) => ({
+            id: camera.id,
+            name: camera.name,
+          }))}
+        />
+      </ReferenceInput>
+      <DateInput source="snapTimestamp_start" label="Start Date" />
+      <DateInput source="snapTimestamp_end" label="End Date" />
+    </Filter>
+  );
+};
 
 export const MyHistory = (props) => {
   const [open, setOpen] = useState(false); // Initialize open state as false
