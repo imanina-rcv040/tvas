55e0dc9fda3efa02c0f0c4fec82e0d616d5e9e33
Ho Zi Xuan <zixuanho@recogine.com>
2023-06-19 17:28:16 +0800

    feat: update development script

--- .dev/run-server.sh
+++ .dev/run-server.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-echo "WARNING: FOR DEVELOPMENT PURPOSE ONLY! DO NOT USE OR COPY THIS SCRIPT TO PRODUCTION!"
-echo "NOTE: Make sure that you have docker build all the images!"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-
-echo "[1] --- Up server"
 docker-compose -f "$SCRIPT_DIR"/../docker-compose.yml up -d
-
-echo
-echo "Finished setting up the servers. Please check with: docker ps && docker logs"
