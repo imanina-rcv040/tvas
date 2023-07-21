b97efb204156c56951cfa57b2e89616b3bea81b1
Imanina Majeed <imanina@recovision.net>
2023-07-21 17:53:33 +0800

    refactor: Remove  custom styling

--- frontend/src/page/Dashboard/component/chart/MyViolationCount.js
+++ frontend/src/page/Dashboard/component/chart/MyViolationCount.js
@@ -2,31 +2,6 @@
 import { useState, useEffect } from "react";
 import { Card, Typography } from "@mui/material";
 
-const styles = {
-  card: {
-    backgroundColor: "#f7f7f7",
-    borderRadius: "12px",
-    padding: "24px",
-    boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.15)",
-    textAlign: "center",
-    height: 400, // Add height property here
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    color: "#222",
-    fontWeight: "bold",
-    fontSize: "28px",
-  },
-  count: {
-    color: "#555",
-    fontSize: "48px",
-    // marginTop: "12px",
-  },
-};
-
 export const MyViolationCount = (props) => {
   const backEndPath = props.backEndPath;
   const [totalViolation, setTotalViolation] = useState([]);
@@ -55,19 +30,18 @@ export const MyViolationCount = (props) => {
   }, []);
 
   return (
-    <Card sx={styles.card}>
+    <Card className="card">
       <Typography
         variant="h4"
         component="h2"
-        className="dashboard-title"
-        margin="0"
+        className="dashboard chart-title"
       >
         Detected Violation
       </Typography>
       <Typography
         variant="h5"
         component="h2"
-        className="dashboard-title"
+        className="dashboard chart-title"
         color="gray"
       >
         Today
@@ -76,7 +50,6 @@ export const MyViolationCount = (props) => {
         variant="h1"
         component="p"
         color="red"
-        className="dashboard-report"
         flexDirection="row"
       >
         {totalViolation.violationCount}
